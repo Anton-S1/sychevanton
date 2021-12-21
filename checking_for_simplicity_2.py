c183ee47b0863b6cb4b2c400cbdfb68bbf592541
Anton-S1 <90714533+Anton-S1@users.noreply.github.com>
2021-12-21 22:53:02 +0300

    Update checking_for_simplicity_2.py

--- checking_for_simplicity_2.py
+++ checking_for_simplicity_2.py
@@ -1,36 +1,24 @@
-n = int(input("Введите число для проверки: "))
 import math
 import random
-def modulo(base, exponent, mod):
-    x = 1
+n = int(input("Введите число для проверки: "))
 
-    y = base;
 
-    while (exponent > 0):
+def calculateJacobian(a, n):
+    if (a == 0):
+        return 0
 
-        if (exponent % 2 == 1):
-            x = (x * y) % mod
+    ans = 1
 
-        y = (y * y) % mod;
+    if (a < 0):
+        a = -a
 
-        exponent = exponent // 2
+        if (n % 4 == 3):
+            ans = -ans
 
-    return x % mod
-for i in range(1,100):
-    if n % 2 == 0:
-        print(False)
-        break
-    a = random.randint(2,n-1)
-    f = math.gcd(n,a)
-    if f > 1:
-        print("False")
-        break
-    def calculateJacobian(a, n):
+    if (a == 1):
+        return ans
 
-        if (a == 0):
-            return 0
-        
-        ans = 1
+    while (a):
 
         if (a < 0):
             a = -a
@@ -38,39 +26,55 @@ for i in range(1,100):
             if (n % 4 == 3):
                 ans = -ans
 
-        if (a == 1):
-            return ans
+        while (a % 2 == 0):
 
-        while (a):
+            a = a // 2
 
-            if (a < 0):
-                a = -a
-                
-                if (n % 4 == 3):
-
-                    ans = -ans
-
-            while (a % 2 == 0):
-
-                a = a // 2
-
-                if (n % 8 == 3 or n % 8 == 5):
-                    ans = -ans
-
-            a, n = n, a
-
-            if (a % 4 == 3 and n % 4 == 3):
+            if (n % 8 == 3 or n % 8 == 5):
                 ans = -ans
 
-            a = a % n
+        a, n = n, a
 
-            if (a > n // 2):
-                a = a - n
+        if (a % 4 == 3 and n % 4 == 3):
+            ans = -ans
 
-        if (n == 1):
-            return ans
+        a = a % n
 
-        return 0;
+        if (a > n // 2):
+            a = a - n
+
+    if (n == 1):
+        return ans
+
+    return 0
+def modulo(base, exponent, mod):
+    x = 1
+
+    y = base
+
+    while (exponent > 0):
+
+        if (exponent % 2 == 1):
+            x = (x * y) % mod
+
+        y = (y * y) % mod
+
+        exponent = exponent // 2
+
+    return x % mod
+
+
+for i in range(1, 100):
+    if n % 2 == 0:
+        print(False)
+        break
+    a = random.randint(2, n - 1)
+    f = math.gcd(n, a)
+    if f > 1:
+        print("False")
+        break
+    calculateJacobian(a, n)
+
     jacobian = (n + calculateJacobian(a, n)) % n
 
     mod = modulo(a, (n - 1) / 2, n)
@@ -81,4 +85,3 @@ for i in range(1,100):
     else:
         print("True")
         break
-
