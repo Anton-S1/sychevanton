51cb2aa151321dbbd97bea05507ee3ee824d6735
Anton-S1 <90714533+Anton-S1@users.noreply.github.com>
2021-12-24 19:38:12 +0300

    Update checking_for_simplicity.py

--- checking_for_simplicity.py
+++ checking_for_simplicity.py
@@ -1,12 +1,21 @@
 import random
+n = int(input("Введите число для проверки: "))
 def ferma(n):
     if n > 2:
         for i in range(25):
             a = random.randint(2, n - 1)
-            if pow(p, n-1, n) != 1:
+            if pow(a, n-1, n) != 1:
                 return False
     return True
-print("Введите число для проверки: ")
-p = int(input())
-print(ferma(p))
+print(ferma(n))
 print("\n")
+
+def is_prime(n):  # Проверим через деление до корня n
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5 + 1)):
+        if n % i == 0:
+            return False
+    else:
+        return True
+print("Проверка через деление до корня:",is_prime(n))
