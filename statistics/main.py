3fc1d8c4342cc43f7cbf14e272649eac67ee98dc
Anton-S1 <90714533+Anton-S1@users.noreply.github.com>
2022-03-24 22:09:51 +0300

    Add files via upload

---
+++ statistics/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+df = pd.read_csv('D.csv')
+
+df.plot(kind='bar')
+
+
+df.plot.kde()
+
+print(stats.kstest('norm', 'norm', N=3))
+print(stats.kstest('norm', 'norm', N=500))
+print()
+
+print(stats.kstest(df, 'norm', (df.mean(), df.std()), N=len(df)))
+plt.show()
